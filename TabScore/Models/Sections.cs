0a66a855355eb97e1049aabbcee1d68ab2e56aa6
Peter Flippant <peterflippant@ntlworld.com>
2019-02-27 09:02:40 +0000

    Version 1.2.1.4

--- TabScore/Models/Sections.cs
+++ TabScore/Models/Sections.cs
@@ -11,18 +11,19 @@ namespace TabScore.Models
 
             using (OdbcConnection connection = new OdbcConnection(DB))
             {
-                SectionClass s = new SectionClass();
-
                 string SQLString = "SELECT ID, Letter, Tables, MissingPair FROM Section";
                 OdbcCommand cmd = new OdbcCommand(SQLString, connection);
                 connection.Open();
                 OdbcDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    s.ID = reader.GetInt32(0);
-                    s.Letter = reader.GetString(1);
-                    s.Tables = reader.GetInt32(2);
-                    s.MissingPair = reader.GetInt32(3);
+                    SectionClass s = new SectionClass
+                    {
+                        ID = reader.GetInt32(0),
+                        Letter = reader.GetString(1),
+                        Tables = reader.GetInt32(2),
+                        MissingPair = reader.GetInt32(3)
+                    };
                     sList.Add(s);
                 }
                 reader.Close();
